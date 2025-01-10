4a53d34b4214d2fe1be7890bacf7ce8a84ab2d4d
Özge Kurnaz <ozge.k60@hotmail.com>
2025-01-10 23:10:19 +0300

    Added tableview delegates

--- Todoey/ViewController.swift
+++ Todoey/ViewController.swift
@@ -8,12 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController{
+    
+    var itemArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
     }
+    
+    @objc func addItem(){
+        let ac = UIAlertController(title: "Add new ToDo item", message: nil, preferredStyle: .alert)
+        
+        ac.addTextField{(alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            
+        }
+        
+        let submitItem = UIAlertAction(title: "Add Item", style: .default) {
+            [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else {return}
+            self?.submit(answer)
+        }
+        ac.addAction(submitItem)
+        present(ac, animated: true)
+        
+    }
+    
+    func submit(_ item: String){
+        itemArray.insert(item, at: itemArray.count )
+        tableView.reloadData()
+    }
+    
+    //MARK- Tableview DataSource Method
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(indexPath.row)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+
+        }
+
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+    
+    
 
 
 }
